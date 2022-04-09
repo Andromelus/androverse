47aac4e1c9b4e05d91d72e4763e36bf67c686d09
florianc <castelainflorian44@gmail.com>
2022-04-09 17:32:57 +0200

    feat: naive approach to player move

--- js/player/player.js
+++ js/player/player.js
@@ -20,8 +20,8 @@ class Player {
         const current_y = this.marker.getLatLng()["lat"]
         const current_x = this.marker.getLatLng()["lng"]
         // if same x or y, then difference is 0. Then we just set it to 0, as we do not move on this axis
-        const direction_y = (current_y - y_target) != 0 ? (current_y - y_target) / (current_y - y_target) : 0
-        const direction_x = (current_x - x_target) != 0 ? (current_x - x_target) / (current_x - x_target) : 0
+        const direction_y = Math.sign(y_target - current_y)
+        const direction_x = Math.sign(x_target - current_x)
 
         this.move_interval_id = setInterval(this.__move, 1000, y_target, x_target, this.marker, this.speed, direction_y, direction_x)
     }
@@ -33,39 +33,39 @@ class Player {
         let reached_y = false
         let next_y = current_y + (speed * y_direction)
         let next_x = current_x + (speed * x_direction)
-        if (y_direction < 0) {
-            if (next_y < y_target) {
-                next_y = y_target
-                reached_y = true
-            }
-        } else if (y_direction > 0) {
+        console.log(current_y, current_x)
+        console.log(y_target, x_target)
+        console.log(next_y, next_x)
+        console.log(y_direction, x_direction)
+        if (y_direction > 0) {
             if (next_y > y_target) {
                 next_y = y_target
                 reached_y = true
             }
-        } else {
-            reached_y = true
+        } else if (y_direction < 0) {
+            if (next_y < y_target) {
+                next_y = y_target
+                reached_y = true
+            }
         }
 
-        if (x_direction < 0) {
-            if (next_x < x_target) {
-                next_x = x_target
-                reached_x = true
-            }
-        } else if (x_direction > 0) {
+        if (x_direction > 0) {
             if (next_x > x_target) {
                 next_x = x_target
                 reached_x = true
             }
-        } else {
-            reached_x = true
+        } else if (x_direction < 0) {
+            if (next_x < x_target) {
+                next_x = x_target
+                reached_x = true
+            }
         }
-        console.debug(current_y, current_x)
-        console.debug(next_y, next_x)
+
         marker.setLatLng([next_y, next_x])
         if (reached_y && reached_x) {
             clearInterval(player_object.move_interval_id)
-        }
+        } 
+
     }
 
     popupButtons = `
